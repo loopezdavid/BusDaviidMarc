fc5a83461194a72f49e827ff9b9f00a7e6902f53
Marc Montoya <marc200113@gmail.com>
2025-09-19 16:34:57 +0200

    Cliente terminado

---
+++ cliente.py
@@ -0,0 +1,13 @@
+class Cliente: 
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+    
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido
+    def setNombre(self, nombre):
+        self.nombre = nombre
+    def setApellido(self, apellido):
+        self.apellido = apellido
