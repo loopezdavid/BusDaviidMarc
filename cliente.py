967e6823ce7c2eec64cf9e997d347b48dbeda5d3
Marc Montoya <marc200113@gmail.com>
2025-09-19 16:55:01 +0200

    akdfsnsd

--- cliente.py
+++ cliente.py
@@ -5,9 +5,13 @@ class Cliente:
     
     def getNombre(self):
         return self.nombre
+        
     def getApellido(self):
         return self.apellido
+        
     def setNombre(self, nombre):
         self.nombre = nombre
+        
     def setApellido(self, apellido):
         self.apellido = apellido
+        
